f82c1a04e298760ce097bfbb2d61002e1fd606e5
Johann MARTNET <martinet.johann@protonmail.ch>
2019-02-20 11:15:01 +0100

    Add st_unittest.c/.h sources

---
+++ src/softtest/st_unittest.c
@@ -0,0 +1,64 @@
+/* Tumbleweed
+Copyright © 2019 Johann MARTINET
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "st_unittest.h"
+
+static UnitTest *current;
+
+struct UnitTest softtest_init_unit_test(const char *file, const char *func, const int line)
+{
+        struct UnitTest unittest;
+        unittest.file     = file;
+        unittest.func     = func;
+        unittest.line     = line;
+        unittest.passed   = true;
+        unittest.exit     = false;
+        unitTest.finished = false;
+        timespec_get(unittest.start, TIME_UTC);
+}
+
+void softtest_set_current_unit_test(struct UnitTest *unittest)
+{
+        current = unittest;
+}
+
+struct UnitTest *softtest_get_current_unit_test(void)
+{
+        return current;
+}
+
+void softtest_end_unit_test(struct UnitTest *unittest)
+{
+        timespec_get(unittest->end, TIME_UTC);
+        unittest.finished = true;
+}
+
+double softtest_unit_test_elapsed_time(struct UnitTest *unittest)
+{
+        if (unittest.finished) {
+                double elapsed = (unittest.end.tv_sec - unittest.start.tv_sec) +
+                                 (unittest.end.tv_nsec - unittest.start.tv_nsec) / 1.0E9;
+                return elapsed;
+        } else {
+                return -1
+        }
+}
---
+++ src/softtest/st_unittest.h
@@ -0,0 +1,41 @@
+/* Tumbleweed
+Copyright © 2019 Johann MARTINET
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <stdbool.h>
+#include <time.h>
+
+struct UnitTest {
+        const char *file;
+        const char *func;
+        const int line;
+        bool passed;
+        bool finished;
+        bool exit;
+        struct timespec start;
+        struct timespec end;
+};
+
+struct UnitTest softtest_init_unit_test(const char *file, const char *func, const int line);
+void softtest_set_current_unit_test(struct UnitTest *unittest);
+struct UnitTest *softtest_get_current_unit_test(void);
+void softtest_end_unit_test(struct UnitTest *unittest);
+double softtest_unit_test_elapsed_time(struct UnitTest *unittest);
